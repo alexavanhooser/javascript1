6c14cadc5ec0ed740079af2ffa101012d08ff7ca
Alexa VanHooser <alexavanhooser@gmail.com>
2018-01-18 20:59:49 -0800

    learned about primitives and objects

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+var sentence = "dfjnsdjfnkjs";
+console.log(sentence);
+{//Block A
+  {//Block B
+    {//Block C
+      {//Block D
+
+      }
+    }
+  }
+}
+
+{let firstNumber = 22; //Declaration and definition
+  console.log(firstNumber);
+}
+var secondNumber = 11;
+// or
+var firstNumber; //declaration
+  firstNumber = 22; //definition
+
+firstNumber = "hello government"; // this redefines the variable (woohoo)
+  console.log(firstNumber);
+
+const truth = "trump sucks";
+console.log(truth); //constant
+
+let rightNow = new Date ();
+console.log(rightNow); //gives you current date/time
+
+
+
+var iLikePi = Math.PI;
+console.log(iLikePi);//this logs a value of pi rounded off
+
+
+
+var string = "yeah boi";
+
+var newString = new String("yeah boi");
+
+console.log(string.toUpperCase());
