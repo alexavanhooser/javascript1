c882dfb9bb00520aa634dab89623b2d3134b9446
Alexa VanHooser <alexavanhooser@gmail.com>
2018-01-30 20:41:30 -0800

    goofed up my branch, i fixed it now

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,3 @@
-// var arr = ["thingOne", 22, true, {}, []];
-// console.log(arr[1]);
-
-//Your favorite movie of 2017
-// var myArr = ["Coco", 9, true, 2018];
-// //console.log(coolThing, coolThing.length);
-//
-// //console.log(typeof(myArr));
-// //console.log(Array.isArray(myArr)); //returns boolean
-//
-// var bestTeacher = prompt('Who is the best teacher?', 'Ryeker');
-// myArr.push(bestTeacher);
-// console.log(myArr)
-//
-// //push and unshift change the original array.
-// //pop and shift do the same thing (changing the array)
-// //BUT: they also give you access to the thing that you removed.
-// //----------------------------splice--------------------------------------
-// //this cuts off everything from index 2 to the end
-// var removedItem = myArr.splice(2);
-// //this cuts off second index; the 1 implies that you're only deleting the one thing.
-// var removedItem = myArr.splice(2, 1);
-// //this would cut the true boolean and insert the false
-// var removedItem = myArr.splice(2, 1, false);
-// //this just inserts the false value at the second index
-// //true gets pushed down the line
-// var removedItem = myArr.splice(2, 0, false);
-// //goes two in from the end and deletes one index.
-// var removedItem = myArr.splice(-2, 1);
-//
-// //----------------slice---------------------
-// //you should see one copy of the same thing
-// var removedItem = myArr.slice();
-// //this is telling it to start at the second index and make a copy
-// var removedItem = myArr.slice(2);
-// //this takes the 0 index and the 1 index, but not the 2 index
-// var removedItem = myArr.slice(0, 2);
-
-
-//just removes
-//myArr.pop();
-//console.log(myArr);
 
 //-----------------------take home--------------------------
 //In your scripts.js file, create 5 arrays, each with one of your top 5 movies' title and the main character's name
@@ -94,15 +52,6 @@ for(var i = 0 ; i < favMovies.length; i++ ) {
 
 
 
-
-
-
-
-
-
-
-
-
 // for loops EX 2
 // for(var i = favMovies.length - 1; i > 0; i--) {
 //   console.log(favMovies[i]);
@@ -114,3 +63,11 @@ for(var i = 0 ; i < favMovies.length; i++ ) {
 //   console.log(favMovies[i]);
 //   i++;
 // } while (i < favMovies.length);
+
+On the day4 branch of your take-home repository, write a program that:
+
+//Creates an array of things you need or want to do this weekend by collecting three todos from the user.
+//Using a FOR loop, change the items in the array by adding days they should be completed to each index.
+//Using a WHILE loop, find the task that will take the longest and log it to the console.
+//Using a DO WHILE loop, add the string 'done' to all the tasks other than the one that will take the longest
+//Finally, alert all the tasks that are marked done in the todo array.
