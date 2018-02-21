e4339b832f1a34bd1b8c78c47b23028db978eed2
Alexa VanHooser <alexavanhooser@gmail.com>
2018-02-20 18:09:49 -0800

    added day 10 stuff

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function log(string) {
+  alert(string);
+};
+
+//callback function
+var myTimer = setTimeout(function() {
+  log("Buy my Russian hacker goods");
+}, 1500) //calls in milliseconds
+
+//clearTimeout
+if(user.login == true) {
+  clearTimeout(myTimer)
+}
+
+log("ya boi is first");
+
+//setInterval 
